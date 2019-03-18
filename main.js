ef729c882b523307bf2fcffd716143ae619e398f
Jamal Ames <Jamal.ames@gmail.com>
2019-03-18 10:39:57 -0400

    1. Can add and remove the to dos

--- main.js
+++ main.js
@@ -17,25 +17,25 @@ function init() {
     // Add event listener functions that get called whenever a user interacts
     // with the respective element.
 
-    document.querySelector('')
+    document.querySelector('#add-todo-button')
         .addEventListener('click', addTodo);
-    
-    document.querySelector('')
+
+    document.querySelector('#remove-todo-button')
         .addEventListener('click', removeTodo);
 
-    document.querySelector('')
+    document.querySelector('#complete-todo-button')
         .addEventListener('click', completeTodo);
 
-    document.querySelector('')
+    document.querySelector('#clear-todos-button')
         .addEventListener('click', clearTodos);
 
-    document.querySelector('')
+    document.querySelector('#remove-completed-button')
         .addEventListener('click', removeCompleted);
 
-    document.querySelector('')
+    document.querySelector('#mark-uncomplete-button')
         .addEventListener('click', markUncomplete);
 
-    document.querySelector('')
+    document.querySelector('#clear-completed-button')
         .addEventListener('click', clearComplete);
 }
 
@@ -50,152 +50,149 @@ function init() {
 function addTodo(event) {
     // Make sure page doesn't reload on button press.
     event.preventDefault();
+    const listItem = document.querySelector('#new-todo').value;
+    todos.push(listItem);
+    console.log(listItem);
+    updateTodosOl();
+    document.querySelector('#new-todo').innerText = listItem.value; // Replace that with the number of items!
 
-    // Grab value of todo input box.
-
-    // Put that value at the end of our list.
-
-    // Update our html.
-
-    // Reset all input fields.
+    resetAllInputs();
 }
 
 function removeTodo(event) {
     // Make sure page doesn't reload on button press.
     event.preventDefault();
+    const index = document.querySelector('#todo-removal-index').value;
 
-    // Grab value that's in user's removal index input box.
+    todos.splice(index, 1);
+    updateTodosOl();
+    document.querySelector('#todo-removal-index').innerText = index.value; // Replace that with the number of items!
 
-    // Remove todo at that index.
-
-    // Update our html.
-
-    // Reset all input fields.
+    resetAllInputs();
 }
 
 function completeTodo(event) {
     // Make sure page doesn't reload on button press.
     event.preventDefault();
-
-    // Grab value that's in user's todo completion index input box.
-
-    // Move todo at that index to the completed list.
-
-    // Update our html.
-
-    // Reset all input fields.
-}
-
-function clearTodos(event) {
-    // Make sure page doesn't reload on button press.
-    event.preventDefault();
-
-    // Clear all todos from the list.
-
-    // Update our html.
-}
-
-function removeCompleted(event) {
-    // Make sure page doesn't reload on button press.
-    event.preventDefault();
-
-    // Grab value that's in user's removal index input box.
-
-    // Remove todo at that index.
-
-    // Update our html.
-
-    // Reset all input fields.
-}
-
-function markUncomplete(event) {
-    // Make sure page doesn't reload on button press.
-    event.preventDefault();
-
-    // Grab value that's in user's todo completion index input box.
-
-    // Move todo at that index to the completed list.
-
-    // Update our html.
-
-    // Reset all input fields.
-}
-
-function clearComplete(event) {
-    // Make sure page doesn't reload on button press.
-    event.preventDefault();
-
-    // Clear all todos from the list.
-
-    // Update our html.
-}
-
-
-/*
-# ========================================================
-# = HTML Management
-# ========================================================
-*/
-
-
-// Use this function to reset all input fields.
-function resetAllInputs() {
-    // Find all input fields.
-    const inputs = document.querySelectorAll('input');
-    
-    // For each one, set its current value to an empty string.
-    for(let i = 0; i < inputs.length; i++) {
-        inputs[i].value = '';
+    const words = sentence.split(' ');
+    let longestWord = '';
+    for (let i = 1; i < words.length; i++) {
+        const currentWord = words[i];
+        if (currentWord.length > longestWord.length) {
+            longestWord = currentWord;
+        }
     }
-}
 
-// Use this function to update the todos ol to reflect the state of our todos
-// list.
-function updateTodosOl() {
-    // Grab the todos ol.
-    const ol = document.querySelector('#todos-list');
-    // Clear it of children nodes.
-    _clearOl(ol);
-    // Re-populate it with everything from the todos array.
-    _addItemsToOl(todos, ol);
-}
 
-// Use this function to update the completed ol to reflect the state of our completed
-// list.
-function updateCompletedOl() {
-    // Grab the completed ol.
-    const ol = document.querySelector('#completed-list');
-    // Clear it of children nodes.
-    _clearOl(ol);
-    // Re-populate it with everything from the completed array.
-    _addItemsToOl(completed, ol);
-}
+    function clearTodos(event) {
+        // Make sure page doesn't reload on button press.
+        event.preventDefault();
+        const emptyList = [];
+        todos = emptyList;
 
-// Clear all children of the given ol.
-// Used INTERNALLY by the ol-updating functions above.
-function _clearOl(ol) {
-    // As long as our ol isn't empty, shift off the first node.
-    while(ol.hasChildNodes()) {
-        ol.removeChild(ol.firstChild);
     }
-}
 
-// Add all items given to the ol given.
-// Used INTERNALLY by the ol-updating functions above.
-function _addItemsToOl(items, ol) {
-    for(let i = 0; i < items.length; i++) {
-        // For every item in the list, add it to the given ol.
-        _addItemToOl(items[i], ol);
+    function removeCompleted(event) {
+        // Make sure page doesn't reload on button press.
+        event.preventDefault();
+
+        // Grab value that's in user's removal index input box.
+
+        // Remove todo at that index.
+
+        // Update our html.
+
+        // Reset all input fields.
     }
-}
 
-// Append any item given to the given ol.
-// Used INTERNALLY by _addItemsToOl
-function _addItemToOl(item, ol) {
-    // Make a new li.
-    const newLi = document.createElement('li');
-    // Add our item to it.
-    newLi.innerText = item;
-    // Append it to the given ol.
-    ol.appendChild(newLi);
-}
+    function markUncomplete(event) {
+        // Make sure page doesn't reload on button press.
+        event.preventDefault();
+
+        // Grab value that's in user's todo completion index input box.
+
+        // Move todo at that index to the completed list.
+
+        // Update our html.
+
+        // Reset all input fields.
+    }
+
+    function clearComplete(event) {
+        // Make sure page doesn't reload on button press.
+        event.preventDefault();
+
+        // Clear all todos from the list.
+
+        // Update our html.
+    }
+
+
+    /*
+    # ========================================================
+    # = HTML Management
+    # ========================================================
+    */
+
+
+    // Use this function to reset all input fields.
+    function resetAllInputs() {
+        // Find all input fields.
+        const inputs = document.querySelectorAll('input');
+
+        // For each one, set its current value to an empty string.
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].value = '';
+        }
+    }
+
+    // Use this function to update the todos ol to reflect the state of our todos
+    // list.
+    function updateTodosOl() {
+        // Grab the todos ol.
+        const ol = document.querySelector('#todos-list');
+        // Clear it of children nodes.
+        _clearOl(ol);
+        // Re-populate it with everything from the todos array.
+        _addItemsToOl(todos, ol);
+    }
+
+    // Use this function to update the completed ol to reflect the state of our completed
+    // list.
+    function updateCompletedOl() {
+        // Grab the completed ol.
+        const ol = document.querySelector('#completed-list');
+        // Clear it of children nodes.
+        _clearOl(ol);
+        // Re-populate it with everything from the completed array.
+        _addItemsToOl(completed, ol);
+    }
+
+    // Clear all children of the given ol.
+    // Used INTERNALLY by the ol-updating functions above.
+    function _clearOl(ol) {
+        // As long as our ol isn't empty, shift off the first node.
+        while (ol.hasChildNodes()) {
+            ol.removeChild(ol.firstChild);
+        }
+    }
+
+    // Add all items given to the ol given.
+    // Used INTERNALLY by the ol-updating functions above.
+    function _addItemsToOl(items, ol) {
+        for (let i = 0; i < items.length; i++) {
+            // For every item in the list, add it to the given ol.
+            _addItemToOl(items[i], ol);
+        }
+    }
+
+    // Append any item given to the given ol.
+    // Used INTERNALLY by _addItemsToOl
+    function _addItemToOl(item, ol) {
+        // Make a new li.
+        const newLi = document.createElement('li');
+        // Add our item to it.
+        newLi.innerText = item;
+        // Append it to the given ol.
+        ol.appendChild(newLi);
+    }
